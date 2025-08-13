7ca5b40b051ceb4a3d6b923e0845aec308ee522c
Skulak13 <165684876+Skulak13@users.noreply.github.com>
2025-08-14 01:18:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function aliasGen(first, last){
+  const firstAlias = first[0].toUpperCase();
+  const lastAlias = last[0].toUpperCase();
+  
+  return /[A-Z]/.test(firstAlias) && /[A-Z]/.test(lastAlias) ? firstName[firstAlias] + " " + surname[lastAlias] : "Your name must start with a letter from A - Z.";
+}
